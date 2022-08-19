022eb08ea0ad5e4d14014be88405bba2f09fb2ad
Alex B <acbird.ab@gmail.com>
2022-08-19 15:20:25 -0600

    refactor snake

--- Snake/scoreboard.py
+++ Snake/scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -11,13 +12,16 @@ class Scoreboard(Turtle):
         self.update_score()
         
     def update_score(self):
-        self.write(f"Score: {self.score}", False, align="center", font=("Arial", 18, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align="center", font=("Arial", 18, "normal"))
         
     def point(self):
         self.score += 1
-        self.clear()
         self.update_score()
         
     def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER!", False, align="center", font=("Arial", 18, "normal"))
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write(f"Score: {self.score} High Score: {self.high_score}", False, align="center", font=("Arial", 18, "normal"))
+        self.score = 0
+        self.update_score()
--- Snake/snake.py
+++ Snake/snake.py
@@ -16,6 +16,13 @@ class Snake:
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
+    
+    def reset(self):
+        for seg in self.squares:
+            seg.goto(1000,1000)
+        self.squares.clear()
+        self.create_snake()
+        self.head = self.squares[0]
             
     def add_segment(self, position):
         new_square = Turtle(shape="square")
