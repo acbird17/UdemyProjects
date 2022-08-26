6e43594e0e3eca856dcc7c14c9cd457abe41cb8c
Alex B <acbird.ab@gmail.com>
2022-08-25 20:06:22 -0600

    pomodoro window fix

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -6,9 +6,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 5
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
@@ -48,14 +48,19 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
-        timer = window.after(10, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
+        raise_above_all(window)
         start_button()
         marks=""
         work_session = math.floor(reps/2)
         for _ in range(work_session):
             marks += "✔"
         check_label.config(text=marks)
+        
+def raise_above_all(window):
+    window.attributes('-topmost', 1)
+    window.attributes('-topmost', 0)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
